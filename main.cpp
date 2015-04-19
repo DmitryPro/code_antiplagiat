f2aa03729a2f2cf597370726133aa5b2b0078bd0
Pavel Gramovich <pavelgramovich@mail.ru>
2015-04-19 14:36:03 +0300

    rename identifiers added

--- main.cpp
+++ main.cpp
@@ -8,20 +8,69 @@ void readText(istream& in, vector<string>& text) {
 		text.push_back(s);
 }
 
-long long hashFunction(string s) {
+void readKeywords(istream& in, set<string>& keywords) {
+    keywords.clear();
+
+	string s;
+	while (in >> s)
+		keywords.insert(s);
+}
+
+void readData(vector<string>& text, set<string>& keywords) {
+    ifstream fin;
+
+    fin.open("input.txt");
+	readText(fin, text);
+	fin.close();
+
+	fin.open("keywords.txt");
+	readKeywords(fin, keywords);
+	fin.close();
+}
+
+long long hashFunction(const string& s) {
 	long long hash = 0;
 	for (size_t i = 0; i < s.length(); i++)
 		hash = (hash * 257 + s[i]) % 1000000007;
 	return hash;
 }
 
+bool isIdentifier(const string& word) {
+    bool flag = true;
+    for (size_t i = 0; i < word.length(); i++)
+        if (!(((word[i] >= '0') && (word[i] <= '9')) ||
+              ((word[i] >= 'a') && (word[i] <= 'z')) ||
+              ((word[i] >= 'A') && (word[i] <= 'Z')) ||
+              word[i] == '_'))
+            flag = false;
+    if ((word[0] >= '0') && (word[0] <= '9'))
+        flag = false;
+    return flag;
+}
+
+void renameIdentifiers(string& oneLineText, const set<string>& keywords) {
+    istringstream in(oneLineText);
+
+    string newLine = "";
+    ostringstream out(newLine);
+
+    int count = 0;
+    string word;
+    while (in >> word) {
+        if (keywords.find(word) == keywords.end() && isIdentifier(word))
+            out << "ID" << count++ << " ";
+        else
+            out << word << " ";
+    }
+
+    oneLineText = out.str();
+}
+
 int main() {
 
     vector<string> text;
-
-	ifstream fin("input.txt");
-	readText(fin, text);
-	fin.close();
+    set<string> keywords;
+    readData(text, keywords);
 
 	formatText(text);
 
@@ -29,7 +78,11 @@ int main() {
 	for (size_t i = 0; i < text.size(); i++)
 		oneLineText += text[i];
 
-	cout << oneLineText;
+	cout << hashFunction(oneLineText) << endl;
+
+    renameIdentifiers(oneLineText, keywords);
+    cout << oneLineText << endl;
+
 
 	return 0;
 }
