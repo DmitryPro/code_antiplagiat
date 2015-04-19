ba91a4f639fe602873c0afac246bed09962cdb73
Pavel Gramovich <pavelgramovich@mail.ru>
2015-04-19 15:39:26 +0300

    first solution

--- main.cpp
+++ main.cpp
@@ -16,18 +16,6 @@ void readKeywords(istream& in, set<string>& keywords) {
 		keywords.insert(s);
 }
 
-void readData(vector<string>& text, set<string>& keywords) {
-    ifstream fin;
-
-    fin.open("input.txt");
-	readText(fin, text);
-	fin.close();
-
-	fin.open("keywords.txt");
-	readKeywords(fin, keywords);
-	fin.close();
-}
-
 long long hashFunction(const string& s) {
 	long long hash = 0;
 	for (size_t i = 0; i < s.length(); i++)
@@ -98,24 +86,97 @@ string makeFingerprint(const string& oneLineText, const set<string>& keywords) {
     return out.str();
 }
 
+int lcs(const string& a, const string& b) {
+    int n = a.length() + 1;
+    int m = b.length() + 1;
+    int* dp = new int[n * m];
+    memset(dp, 0, sizeof(int) * n * m);
+
+    for (int i = 1; i < n; i++)
+        for (int j = 1; j < m; j++) {
+            dp[i * m + j] = max(dp[(i - 1) * m + j], dp[i * m + (j - 1)]);
+            if (a[i - 1] == b[j - 1])
+                dp[i * m + j] = max(dp[i * m + j], dp[(i - 1) * m + (j - 1)] + 1);
+        }
+    return dp[n * m - 1];
+}
+
 int main() {
 
-    vector<string> text;
+    ifstream fin("01/input.txt");
+
+    int n;
+    fin >> n;
+    vector<string> fileNames(n);
+    for (int i = 0; i < n; i++)
+        fin >> fileNames[i];
+
+    fin.close();
+
     set<string> keywords;
-    readData(text, keywords);
 
-	formatText(text);
+	fin.open("keywords.txt");
+	readKeywords(fin, keywords);
+	fin.close();
 
-	string oneLineText = "";
-	for (size_t i = 0; i < text.size(); i++)
-		oneLineText += text[i];
+    vector<string> fingerprints(n);
 
-	int h1 = hashFunction(oneLineText);
-    renameIdentifiers(oneLineText, keywords);
-    int h2 = hashFunction(oneLineText);
-    string fingerprint = makeFingerprint(oneLineText, keywords);
-    int h3 = hashFunction(fingerprint);
-    cout << fingerprint << endl;
+    for (int i = 0; i < n; i++) {
+        vector<string> text;
+
+        fin.open(("01/" + fileNames[i]).c_str());
+        readText(fin, text);
+        fin.close();
+
+        formatText(text);
+
+        string oneLineText = "";
+        for (size_t i = 0; i < text.size(); i++)
+            oneLineText += text[i];
+
+        renameIdentifiers(oneLineText, keywords);
+        fingerprints[i] = makeFingerprint(oneLineText, keywords);
+    }
+
+    set<int> was;
+    vector< vector< int > > ans;
+    for (int i = 0; i < n; i++) {
+        vector<int> q;
+        vector<int> same;
+
+        if (was.find(i) != was.end())
+            continue;
+
+        same.push_back(i);
+        q.push_back(i);
+        while (q.size()) {
+            int p = q.back();
+            q.pop_back();
+
+            if (was.find(p) != was.end())
+                continue;
+            was.insert(p);
+
+            for (int j = 0; j < n; j++)
+                if (was.find(j) == was.end()) {
+                    int len = lcs(fingerprints[p], fingerprints[j]);
+                    if (len >= min(fingerprints[p].length(), fingerprints[j].length()) * 0.8) {
+                        same.push_back(j);
+                        q.push_back(j);
+                    }
+                }
+        }
+
+        if (same.size() > 1)
+            ans.push_back(same);
+    }
+
+    cout << ans.size() << endl;
+    for (size_t i = 0; i < ans.size(); i++) {
+        for (int j = 0; j < ans[i].size(); j++)
+            cout << fileNames[ans[i][j]] << ' ';
+        cout << endl;
+    }
 
 	return 0;
 }
